3315fd6a0921991f8b3252d446ee601166e8bf51
anagha012004 <anaghabadhe3@gmail.com>
2026-04-05 15:31:39 +0530

    change from postgressql:// to jdbc:postgresql://

---
+++ backend/src/main/java/com/financeapi/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package com.financeapi.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    public DataSource dataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+
+        // Render injects postgresql:// but JDBC requires jdbc:postgresql://
+        if (url.startsWith("postgresql://")) {
+            url = "jdbc:postgresql://" + url.substring("postgresql://".length());
+        }
+
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName("org.postgresql.Driver");
+        return ds;
+    }
+}
